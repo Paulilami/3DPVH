ee0c45c8f19d2312979e91e6fac337e7b0c547af
Paul Ilami <108613093+Paulilami@users.noreply.github.com>
2024-05-10 14:44:13 +0200

    Update validation.h

--- validation.h
+++ validation.h
@@ -1,15 +1,12 @@
 #include "validation.h"
 
-// Data structures (replace with your actual data types)
+// 3D endpoint structur defined for sam2 
 struct Point3D {
   double x;
   double y;
   double z;
 };
 
-struct INSData {
-  // ... data fields specific to your Information Source (INS)
-  Point3D point3d;  // The 3D point representing the retrieved information
-};
+struct INSData { Point3D point3d; }
 
 bool validate_data(const std::vector<INSData>& ins_data);
