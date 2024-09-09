c7bc1c6bdc9e0e0b812c03f34a3f39436d3f400f
Paul Ilami <108613093+Paulilami@users.noreply.github.com>
2024-09-09 14:27:51 +0200

    Update communicationBeta.cpp

--- communicationBeta.cpp
+++ communicationBeta.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <string>
 
-// Messages 
 struct Message {
   std::string content;
   // ...sam2...
@@ -18,11 +17,8 @@ struct SecureMessagePacket {
 class SecureCommunicator {
 public:
   SecureCommunicator() {
-    // ... VRF key generation and shared secret establishment using libsodium 
-    //...sam2...
   }
 
-  // Send a secure message to another node 
   bool send_message(const Node& recipient, const Message& message) {
     std::vector<unsigned char> message_hash = compute_message_hash(message);
 
@@ -42,26 +38,24 @@ public:
   }
 
 private:
-  // Compute a message hash 
   std::vector<unsigned char> compute_message_hash(const Message& message) {
     #ifdef SODIUM_CRYPTO_HASH_SHA256
       unsigned char hash[crypto_hash_sha256_BYTES];
       crypto_hash_sha256(hash, message.content.data(), message.content.size());
       return std::vector<unsigned char>(hash, hash + crypto_hash_sha256_BYTES);
     #else
-      // ...sam2-3...
     #endif
   }
 
   bool generate_vrf_and_proof(std::vector<unsigned char>& vro, std::vector<unsigned char>& proof) {
-    return false; // Placeholder
+    return false; 
   }
 
   std::vector<unsigned char> generate_merkle_proof(const std::vector<unsigned char>& message_hash) {
-    return std::vector<unsigned char>(); // Placeholder
+    return std::vector<unsigned char>(); 
   }
 
   bool send_packet(const Node& recipient, const SecureMessagePacket& packet) {
-    return false; // Placeholder
+    return false; 
   }
 };
